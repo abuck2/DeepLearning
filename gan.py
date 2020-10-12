97903d4b1597d53edf6a6127743edfa6e746291f
manchot2 <leosecond@hotmail.com>
2020-10-12 22:00:48 +0200

    Generative adversarial network

--- gan.py
+++ gan.py
@@ -3,23 +3,32 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import SGD, Adam
 import tensorflow as tf
 
+import sys, os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import sys, os
+from skimage.io import imread
 
 
 
 class GanNetwork:
     def __init__(self):
+        print("Initialization...")
+
         # Load in the data
         self.mnist = tf.keras.datasets.mnist
         
         # Dimensionality of the latent space
         self.latent_dim = 100
 
-    def dataprep(self):
+        # Config
+        self.batch_size = 32
+        self.epochs = 300
 
+
+
+    def dataprep(self):
+        print("Preparing the data")
         (x_train, y_train), (x_test, y_test) = self.mnist.load_data()
 
         # map inputs to (-1, +1) for better training
@@ -27,14 +36,16 @@ class GanNetwork:
         print("x_train.shape:", x_train.shape)
 
         # Flatten the data
-        N, H, W = x_train.shape
-        self.D = H * W
+        N, self.H, self.W = x_train.shape
+        self.D = self.H * self.W
         x_train = x_train.reshape(-1, self.D)
         x_test = x_test.reshape(-1, self.D) 
 
         return x_train, y_train, x_test, y_test
 
     def build_generator(self, latent_dim):
+        print("Building the generator")
+
         i = Input(shape=(latent_dim,))
         x = Dense(256, activation=LeakyReLU(alpha=0.2))(i)
         x = BatchNormalization(momentum=0.7)(x)
@@ -48,6 +59,8 @@ class GanNetwork:
         return model
 
     def build_discriminator(self, img_size):
+        print("Building the discriminator")
+        
         i = Input(shape=(img_size,))
         x = Dense(512, activation=LeakyReLU(alpha=0.2))(i)
         x = Dense(256, activation=LeakyReLU(alpha=0.2))(x)
@@ -56,6 +69,8 @@ class GanNetwork:
         return model
 
     def compiler(self):
+        print("Compiling the models")
+
         # Compile both models in preparation for training
 
         # Build and compile the self.discriminator
@@ -81,25 +96,20 @@ class GanNetwork:
         fake_pred = self.discriminator(img)
 
         # Create the combined model object
-        combined_model = Model(z, fake_pred)
+        self.combined_model = Model(z, fake_pred)
 
         # Compile the combined model
-        combined_model.compile(loss='binary_crossentropy', optimizer=Adam(0.0002, 0.5))
+        self.combined_model.compile(loss='binary_crossentropy', optimizer=Adam(0.0002, 0.5))
 
 
     def train(self, x_train, y_train, x_test, y_test):
+        print("Training the models!")
         # Train the GAN
-
-
-        # Config
-        batch_size = 32
-        epochs = 30000
         sample_period = 200 # every `sample_period` steps generate and save some data
 
-
         # Create batch labels to use when calling train_on_batch
-        ones = np.ones(batch_size)
-        zeros = np.zeros(batch_size)
+        ones = np.ones(self.batch_size)
+        zeros = np.zeros(self.batch_size)
 
         # Store the losses
         d_losses = []
@@ -111,18 +121,18 @@ class GanNetwork:
 
         
         # Main training loop
-        for epoch in range(epochs):
+        for epoch in range(self.epochs):
         
             ###########################
             ### Train self.discriminator ###
             ###########################
 
             # Select a random batch of images
-            idx = np.random.randint(0, x_train.shape[0], batch_size)
+            idx = np.random.randint(0, x_train.shape[0], self.batch_size)
             real_imgs = x_train[idx]
 
             # Generate fake images
-            noise = np.random.randn(batch_size, self.latent_dim)
+            noise = np.random.randn(self.batch_size, self.latent_dim)
             fake_imgs = self.generator.predict(noise)
 
             # Train the self.discriminator
@@ -137,28 +147,41 @@ class GanNetwork:
             ### Train self.generator ###
             #######################
 
-            noise = np.random.randn(batch_size, self.latent_dim)
-            g_loss = combined_model.train_on_batch(noise, ones)
+            noise = np.random.randn(self.batch_size, self.latent_dim)
+            g_loss = self.combined_model.train_on_batch(noise, ones)
 
             # do it again!
-            noise = np.random.randn(batch_size, self.latent_dim)
-            g_loss = combined_model.train_on_batch(noise, ones)
+            noise = np.random.randn(self.batch_size, self.latent_dim)
+            g_loss = self.combined_model.train_on_batch(noise, ones)
 
             # Save the losses
             d_losses.append(d_loss)
             g_losses.append(g_loss)
 
             if epoch % 100 == 0:
-                print(f"epoch: {epoch+1}/{epochs}, d_loss: {d_loss:.2f}, \
+                print(f"epoch: {epoch+1}/{self.epochs}, d_loss: {d_loss:.2f}, \
                       d_acc: {d_acc:.2f}, g_loss: {g_loss:.2f}")
 
             if epoch % sample_period == 0:
-                sample_images(epoch)
+                self.sample_images(epoch)
+        
+
+        #plots
+        plt.plot(g_losses, label='g_losses')
+        plt.plot(d_losses, label='d_losses')
+        plt.legend()
+
+        a = imread('gan_images/0.png')
+        plt.imshow(a)
+        
+        a = imread('gan_images/{}.png'.format(self.epochs))
+        plt.imshow(a)
+
 
 
     def sample_images(self, epoch):
         rows, cols = 5, 5
-        noise = np.random.randn(rows * cols, latent_dim)
+        noise = np.random.randn(rows * cols, self.latent_dim)
         imgs = self.generator.predict(noise)
 
         # Rescale images 0 - 1
@@ -168,7 +191,7 @@ class GanNetwork:
         idx = 0
         for i in range(rows):
             for j in range(cols):
-                axs[i,j].imshow(imgs[idx].reshape(H, W), cmap='gray')
+                axs[i,j].imshow(imgs[idx].reshape(self.H, self.W), cmap='gray')
                 axs[i,j].axis('off')
                 idx += 1
         fig.savefig("gan_images/%d.png" % epoch)
@@ -176,6 +199,7 @@ class GanNetwork:
 
 
     def run(self):
+        print("Starting to run")
         x_train, y_train, x_test, y_test = self.dataprep()
         self.compiler()
         self.train(x_train, y_train, x_test, y_test)
